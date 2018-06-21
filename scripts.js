c002bcf1ac95b6050d7d268d1398b8f3a804c78a
NedBark <alexandru_nedea@yahoo.com>
2018-06-21 14:05:14 +0300

    bla

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var prevScrollPos=window.pageYOffset;
+
+window.onscroll = function (prevScrollPos) {
+    var currentScrollPos = window.pageYOffset;
+    if(prevScrollPos>currentScrollPos) {
+        document.getElementById("HidePic")
+
+    }
+}
