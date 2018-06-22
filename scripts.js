a8a31b90033147d85d67138a43ed7e1bb79565f2
NedBark <alexandru_nedea@yahoo.com>
2018-06-22 11:23:39 +0300

    asd

--- scripts.js
+++ scripts.js
@@ -7,12 +7,19 @@ window.onscroll = function (prevScrollPos) {
 
     }
 }
-var x;
-function seeInput(){
+var seeds;
+var fileValues;
+function readSeed(){
     var data=document.getElementById("user_input").value;
     return data
 }
-function Main() {
-    x = seeInput();
-    window.alert(x);
+function readFile(){
+    var data=document.getElementById("datafile").value;
+    return data;
 }
+function Main() {
+    seeds = readSeed();
+    fileValues = readFile();
+    window.alert(fileValues);
+}
+
